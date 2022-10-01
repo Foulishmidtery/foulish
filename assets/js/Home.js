bef28f5aa672f273c760fcc95d1ecc8dad841d33
WilliamSinaga <Jenkinssinaga@gmail.com>
2022-10-01 19:20:14 +0700

    first commit

---
+++ assets/js/Home.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing1", {
+        strings: ["Photos."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing2", {
+        strings: ["to find out who i am."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing3", {
+        strings: ["want to know?"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing4", {
+        strings: ["i've done."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing5", {
+        strings: ["as follows."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing6", {
+        strings: ["see now."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
